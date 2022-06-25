cd05af4ef5ea8016e5c9b46bcb53de7a525e686f
Camiletb <caena@alumni.uv.es>
2022-06-25 19:48:58 +0200

    add canvas

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/* Cuando un navegador no tiene habilitado Javascript con la etiqueta 
+<noscript></noscript> podemos comunicarnos con el cliente. Esto se puede dar en 
+navegadores de documentos gubernamentales, por seguridad.
+*/
 function myToggleFunction() {
   var x = document.getElementById("toggleable");
   if (x.style.display === "none") {
