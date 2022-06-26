08769fbbf693fc6aa27096f22764d0b86a5ac187
Camiletb <caena@alumni.uv.es>
2022-06-26 17:02:06 +0200

    add ampliar button

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 <noscript></noscript> podemos comunicarnos con el cliente. Esto se puede dar en 
 navegadores de documentos gubernamentales, por seguridad.
 */
+var contenedor = document.querySelector("#seccion_canvas");
+var ampliarcanvas = document.querySelector("#canvas_ejemplo");
+
 function myToggleFunction() {
   var x = document.getElementById("toggleable");
   if (x.style.display === "none") {
@@ -9,4 +12,16 @@ function myToggleFunction() {
   } else {
       x.style.display = "none";
   }
+}
+
+function ampliar() {
+  contenedor.style.width = "100%";
+  contenedor.style.height = "100vh";
+  contenedor.style.margin = "0";
+
+  ampliarcanvas.style.width = "100%";
+  ampliarcanvas.style.height = "100vh";
+  // ampliarcanvas.style.backgroundSize = "cover";
+  // ampliarcanvas.style.backgroundRepeat = "no-repeat";
+
 }
